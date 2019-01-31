87bdc9ec0d648773294923ea766efbf40e62c86e
srujith470 <srujith.reddy2012@gmail.com>
2019-01-31 15:11:29 -0500

    FIRST REDUCER LOGIN

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,28 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-
+import { User } from '../model/user.model';
+import { AuthActionTypes } from '../auth/auth.actions';
+type AuthState = {
+  loggedIn : boolean,
+  user: User
+} // this is object with authentication properties 
 export interface AppState {
-  
+    auth: AuthState  // add the authentication properties object to STORE
 }
-
+function authReducer(state:AuthState, action) :AuthState {
+    switch(action.type){
+      case AuthActionTypes.LoginAction:
+      return{
+        loggedIn:true,
+        user: action.payload.user
+      }
+      default: return state
+ // reducer function for authentication to track its state
+    }
+}
 export const reducers: ActionReducerMap<AppState> = {
-
+  auth: authReducer // export authentication reducer
 };
-
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
